6b029ba18f21ba9b5eb52511394570d1dd383a2b
Carsten Anand <email.carsten.anand@gmail.com>
2025-08-16 10:18:55 +0800

    Initial Commit

---
+++ PoseMatcher/PoseMatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoseMatcherApp.swift
+//  PoseMatcher
+//
+//  Created by Carsten Anand on 16/8/25.
+//
+
+import SwiftUI
+
+@main
+struct PoseMatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
